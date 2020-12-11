1822e2085cbb41d08d188c4f73b77f475db1c8ff
MyungWanPark <mywanpark@gmail.com>
2020-12-11 14:34:07 +0900

    updating UpClockwise Function

--- Step3.py
+++ Step3.py
@@ -6,12 +6,11 @@ class Cube:
     # 3x3 큐브 값 입력받아 저장하기
     def __init__(self,row1,row2,row3):
 
-        #self.one_side = [['R', 'R', 'W'], ['G', 'C', 'W'], ['G', 'B', 'B']]
-        self.one_side = [row1, row2, row3]
-
         self.row1 = row1
         self.row2 = row2
         self.row3 = row3
+
+        self.rows = [self.row1, self.row2, self.row3]
 
         self.col1 = []
         self.col2 = []
@@ -21,40 +20,47 @@ class Cube:
         self.temp = []
 
         for i in range(3):
-            self.col1 += [self.one_side[i][0]]
-            self.col2 += [self.one_side[i][1]]
-            self.col3 += [self.one_side[i][2]]
+            self.col1 += [self.rows[i][0]]
+            self.col2 += [self.rows[i][1]]
+            self.col3 += [self.rows[i][2]]
 
+        self.cols = [self.col1, self.col2, self.col3]
     # 큐브 한쪽면 출력하기
-    def print_one_side(self,Front,Right,Left,Up,Down,Back):
+    def print_one_side(*args):
+
+        # command를 실행 후 각 면의 row와 col값이 바뀌었으니, rows와 cols를 업데이트 해줘야 이것을 이용한 출력이 제대로 된다.
+        for i in args:
+            i.rows = [i.row1, i.row2, i.row3]
+            i.cols = [i.col1, i.col2, i.col3]
 
         # 큐브 Up 출력
         for i in range(3):
             print("              ", end = " ")
             for j in range(3):
-                print(Up.one_side[i][j], end=' ')
+                print(Up.rows[i][j], end = " ")
             print()
 
+        # 큐브에서 윗면과 아랫면 제외한 후 출력
         for i in range(3):
 
-            # 큐브 왼쪽부터 출력
+            # 큐브 Left 출력
             for j in range(3):
-                print(Left.one_side[i][j], end=' ')
+                print(Left.rows[i][j], end=' ')
             print("   ",end = " ")
 
             # 큐브 Front 출력
             for j in range(3):
-                print(Front.one_side[i][j], end=' ')
+                print(Front.rows[i][j], end=' ')
             print("   ", end=" ")
 
             # 큐브 Right 출력
             for j in range(3):
-                print(Right.one_side[i][j], end=' ')
+                print(Right.rows[i][j], end=' ')
             print("   ", end=" ")
 
             # 큐브 Back 출력
             for j in range(3):
-                print(Back.one_side[i][j], end=' ')
+                print(Back.rows[i][j], end=' ')
             print("   ", end=" ")
             print()
 
@@ -62,13 +68,13 @@ class Cube:
         for i in range(3):
             print("              ", end = " ")
             for j in range(3):
-                print(Down.one_side[i][j], end=' ')
+                print(Down.rows[i][j], end=' ')
             print()
 
     # 큐브게임 시작하기
-    def start(self,front,Right,Left,Up,Down,Back):
+    def start(self,Front,Right,Left,Up,Down,Back):
 
-        self.print_one_side(front,Right,Left,Up,Down,Back)
+        self.print_one_side(Front,Right,Left,Up,Down,Back)
         print()
 
         while True:
@@ -140,81 +146,101 @@ class Cube:
 
                 func = functions[i]
                 print(i)
-                func()
+
+                # command가 Up과 관련있는 경우, 아래와 같은 파라미터를 넘겨준다.
+                if 'U' in i:
+                    func(Up,Front,Left,Right,Back)
+
+                # command가 Left과 관련있는 경우, 아래와 같은 파라미터를 넘겨준다.
+                if 'L' in i:
+                    func(Up,Front,Left,Down,Back)
+
+                # command가 Front과 관련있는 경우, 아래와 같은 파라미터를 넘겨준다.
+                if 'F' in i:
+                    func(Up,Front,Left,Right,Down)
+
+                # command가 Right과 관련있는 경우, 아래와 같은 파라미터를 넘겨준다.
+                if 'R' in i:
+                    func(Up,Front,Down,Right,Back)
+
+                # command가 Back과 관련있는 경우, 아래와 같은 파라미터를 넘겨준다.
+                if 'B' in i:
+                    func(Up,Down,Left,Right,Back)
+
+                # command가 Down과 관련있는 경우, 아래와 같은 파라미터를 넘겨준다.
+                if 'D' in i:
+                    func(Down,Front,Left,Right,Back)
 
                 # 함수를 실행한 후 전체 출력
-                self.print_one_side(front,Right,Left,Up,Down,Back)
+                self.print_one_side(Front,Right,Left,Up,Down,Back)
                 print()
 
-    """def UpDirLeft(self):
-
-        self.temp = copy.deepcopy(self.row1)
-
-        # self.row1[0] = self.temp[1]
-        # self.row1[1] = self.temp[2]
-        # self.row1[2] = self.temp[0]
-
-        # row1은 3개로 이루어져 있어서 반복을 3번 함
-        for i in range(3):
-            # 움직인 후 마지막 값은 기존의 첫번째 값이 와야한다.
-            if i == 2:
-                self.row1[i] = self.temp[0]
-                break
-            # 한칸씩 옆으로 움직인 값을 넣어준다.
-            self.row1[i] = self.temp[i + 1]"""
-
-    def UpCW(self):
+    def UpCW(self,Up,Front,Left,Right,Back):
         print("UpCW가 실행되었습니다.")
+        # 윗면이 시계방향으로 바뀐다.
+        Up.temp = Up.col3
+        Up.col3 = Up.row1
+        Up.row1 = Up.col1
+        Up.col1 = Up.row3
+        Up.row3 = Up.temp
 
-    def UpCCW(self):
+        #아랫면 빼고 row1이 시계방향으로 바뀐다.
+        Front.temp = Front.row1
+        Front.row1 = Right.row1
+        Right.row1 = Back.row1
+        Back.row1 = Left.row1
+        Left.row1 = Front.temp
+        print("Front.row1 = ",Front.row1)
+
+    def UpCCW(self,Up,Front,Left,Right,Back):
         print("UpCCW가 실행되었습니다.")
 
-    def UpCW2(self):
+    def UpCW2(self,Up,Front,Left,Right,Back):
         print("UpCW2가 실행되었습니다.")
 
-    def LeftCW(self):
+    def LeftCW(self,Up,Front,Left,Down,Back):
         print("LeftCW 실행되었습니다.")
 
-    def LeftCCW(self):
+    def LeftCCW(self,Up,Front,Left,Down,Back):
         print("LeftCCW 실행되었습니다.")
 
-    def LeftCW2(self):
+    def LeftCW2(self,Up,Front,Left,Down,Back):
         print("LeftCW2 실행되었습니다.")
 
-    def FrontCW(self):
+    def FrontCW(self,Up,Front,Left,Right,Down):
         print("FrontCW 실행되었습니다.")
 
-    def FrontCCW(self):
+    def FrontCCW(self,Up,Front,Left,Right,Down):
         print("FrontCCW 실행되었습니다.")
 
-    def FrontCW2(self):
+    def FrontCW2(self,Up,Front,Left,Right,Down):
         print("FrontCW2 실행되었습니다.")
 
-    def RightCW(self):
+    def RightCW(self,Up,Front,Down,Right,Back):
         print("RightCW 실행되었습니다.")
 
-    def RightCCW(self):
+    def RightCCW(self,Up,Front,Down,Right,Back):
         print("RightCCW 실행되었습니다.")
 
-    def RightCW2(self):
+    def RightCW2(self,Up,Front,Down,Right,Back):
         print("RightCW2 실행되었습니다.")
 
-    def BackCW(self):
+    def BackCW(self,Up,Down,Left,Right,Back):
         print("BackCW 실행되었습니다.")
 
-    def BackCCW(self):
+    def BackCCW(self,Up,Down,Left,Right,Back):
         print("BackCCW 실행되었습니다.")
 
-    def BackCW2(self):
+    def BackCW2(self,Up,Down,Left,Right,Back):
         print("BackCW2 실행되었습니다.")
 
-    def DownCW(self):
+    def DownCW(self,Down,Front,Left,Right,Back):
         print("DownCW 실행되었습니다.")
 
-    def DownCCW(self):
+    def DownCCW(self,Down,Front,Left,Right,Back):
         print("DownCCW 실행되었습니다.")
 
-    def DownCW2(self):
+    def DownCW2(self,Down,Front,Left,Right,Back):
         print("DownCW2 실행되었습니다.")
 
 Front = Cube(['O','O','O'],['O','O','O'],['O','O','O'])
