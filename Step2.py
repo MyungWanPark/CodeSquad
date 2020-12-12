d53902674f80afc4d94d09453eaf3f736b314dbf
MyungWanPark <mywanpark@gmail.com>
2020-12-12 13:05:45 +0900

    Update to solve the problem of moving up_left and moving right_down

--- Step2.py
+++ Step2.py
@@ -5,30 +5,28 @@ class Cube:
     # 3x3 큐브 값 입력받아 저장하기
     def __init__(self):
 
-        self.one_side = [['R','R','W'],['G','C','W'],['G','B','B']]
-
-        self.row1 = self.one_side[0]
-        self.row2 = self.one_side[1]
-        self.row3 = self.one_side[2]
-
-        self.col1 = []
-        self.col2 = []
-        self.col3 = []
-
-        # 큐브를 돌릴때 임시로 값을 넣어놓기 위한 리스트
+        self.total = ['R','R','W','G','C','W','G','B','B']
+        self.rows = []
+        self.index = 0
         self.temp = []
 
+    def make_rows(self):
+
+        self.rows.clear()
+        self.index = 0
+
         for i in range(3):
-            self.col1 += [self.one_side[i][0]]
-            self.col2 += [self.one_side[i][1]]
-            self.col3 += [self.one_side[i][2]]
+            self.rows.append(self.total[self.index:self.index+3])
+            self.index += 3
 
     # 큐브 한쪽면 출력하기
     def print_one_side(self):
 
+        self.make_rows()
+
         for i in range(3):
             for j in range(3):
-                print(self.one_side[i][j], end = ' ')
+                print(self.rows[i][j], end = " ")
             print()
 
     # 큐브게임 시작하기
@@ -76,6 +74,8 @@ class Cube:
 
                 func = functions[i]
                 print(i)
+                # 함수 실행 전, 현재의 total을 temp로 복사한다.
+                self.temp = copy.deepcopy(self.total)
                 func()
 
                 # 함수를 실행한 후 전체 출력
@@ -84,111 +84,35 @@ class Cube:
 
     def UpDirLeft(self):
 
-        self.temp = copy.deepcopy(self.row1)
-
-        # self.row1[0] = self.temp[1]
-        # self.row1[1] = self.temp[2]
-        # self.row1[2] = self.temp[0]
-
-        # row1은 3개로 이루어져 있어서 반복을 3번 함
-        for i in range(3):
-            # 움직인 후 마지막 값은 기존의 첫번째 값이 와야한다.
-            if i == 2:
-                self.row1[i] = self.temp[0]
-                break
-            # 한칸씩 옆으로 움직인 값을 넣어준다.
-            self.row1[i] = self.temp[i+1]
+        self.total[0],self.total[1],self.total[2] = self.temp[1],self.temp[2],self.temp[0]
 
     def UpDirRight(self):
 
-        self.temp = copy.deepcopy(self.row1)
-
-        # row1은 3개로 이루어져 있어서 반복을 3번 함
-        for i in range(3):
-            # 움직인 후 첫번째 값은 기존의 마지막 값이 와야한다.
-            if i == 0:
-                self.row1[i] = self.temp[2]
-                continue
-            # 한칸씩 옆으로 움직인 값을 넣어준다.
-            self.row1[i] = self.temp[i - 1]
+        self.total[0], self.total[1], self.total[2] = self.temp[1], self.temp[2], self.temp[0]
 
     def RightDirUp(self):
 
-        self.temp = copy.deepcopy(self.col3)
-
-        # col3은 3개로 이루어져 있어서 반복을 3번 함
-        for i in range(3):
-            # 움직인 후 마지막 값은 기존의 첫번째 값이 와야한다.
-            if i == 2:
-                self.col3[i] = self.temp[0]
-                break
-            # 한칸씩 위로 움직인 값을 넣어준다.
-            self.col3[i] = self.temp[i + 1]
+        self.total[2], self.total[5], self.total[8] = self.temp[5], self.temp[8], self.temp[2]
 
     def RightDirDown(self):
 
-        self.temp = copy.deepcopy(self.col3)
-
-        # col3은 3개로 이루어져 있어서 반복을 3번 함
-        for i in range(3):
-            # 움직인 후 첫번째 값은 기존의 마지막 값이 와야한다.
-            if i == 0:
-                self.col3[i] = self.temp[2]
-                continue
-            # 한칸씩 아래로 움직인 값을 넣어준다.
-            self.col3[i] = self.temp[i - 1]
+        self.total[2], self.total[5], self.total[8] = self.temp[8], self.temp[2], self.temp[5]
 
     def LeftDirDown(self):
 
-        self.temp = copy.deepcopy(self.col1)
-
-        # col1은 3개로 이루어져 있어서 반복을 3번 함
-        for i in range(3):
-            # 움직인 후 첫번째 값은 기존의 마지막 값이 와야한다.
-            if i == 0:
-                self.col1[i] = self.temp[2]
-                continue
-            # 한칸씩 아래로 움직인 값을 넣어준다.
-            self.col1[i] = self.temp[i - 1]
+        self.total[0], self.total[3], self.total[6] = self.temp[6], self.temp[0], self.temp[3]
 
     def LeftDirUp(self):
 
-        self.temp = copy.deepcopy(self.col1)
-
-        # col1은 3개로 이루어져 있어서 반복을 3번 함
-        for i in range(3):
-            # 움직인 후 마지막 값은 기존의 첫번째 값이 와야한다.
-            if i == 2:
-                self.col1[i] = self.temp[0]
-                break
-            # 한칸씩 위로 움직인 값을 넣어준다.
-            self.col1[i] = self.temp[i + 1]
+        self.total[0], self.total[3], self.total[6] = self.temp[3], self.temp[6], self.temp[0]
 
     def BottomDirRight(self):
 
-        self.temp = copy.deepcopy(self.row3)
-
-        # row3은 3개로 이루어져 있어서 반복을 3번 함
-        for i in range(3):
-            # 움직인 후 첫번째 값은 기존의 마지막 값이 와야한다.
-            if i == 0:
-                self.row3[i] = self.temp[2]
-                continue
-            # 한칸씩 옆으로 움직인 값을 넣어준다.
-            self.row3[i] = self.temp[i - 1]
+        self.total[6], self.total[7], self.total[8] = self.temp[8], self.temp[6], self.temp[7]
 
     def BottomDirLeft(self):
 
-        self.temp = copy.deepcopy(self.row3)
-
-        # row3은 3개로 이루어져 있어서 반복을 3번 함
-        for i in range(3):
-            # 움직인 후 마지막 값은 기존의 첫번째 값이 와야한다.
-            if i == 2:
-                self.row3[i] = self.temp[0]
-                break
-            # 한칸씩 옆으로 움직인 값을 넣어준다.
-            self.row3[i] = self.temp[i + 1]
+        self.total[6], self.total[7], self.total[8] = self.temp[7], self.temp[8], self.temp[6]
 
 c1 = Cube()
 c1.start()
